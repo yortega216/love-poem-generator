01cfff2c3e21454264809ddaab9e3b6ae66334b3
yortega216 <yortega216@gmail.com>
2025-03-08 16:38:38 -0600

    Improve UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a love poem about ${instructionsInput}`;
     let context = "You are a romantic expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not include the HTML wording. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong></strong> element at the end of the poem";
     let apiURl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⌛︎ Generating a Love Poem about ${instructionsInput.value}</div>`;
+
 
   console.log("generating poem");
   console.log(`Prompt: ${prompt}`);
